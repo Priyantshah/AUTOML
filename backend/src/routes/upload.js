f905428068c975b78e0908a246748fcefb203fbe
Priyantshah <priyantshah3001@gmail.com>
2025-11-30 16:28:56 +0000

    EDIT

--- backend/src/routes/upload.js
+++ backend/src/routes/upload.js
@@ -1,5 +1,5 @@
 import express from "express";
-import upload from "../middlewares/uploadmiddleware.js";
+
 import { handleUpload } from "../controllers/uploadController.js";
 
 const router = express.Router();
